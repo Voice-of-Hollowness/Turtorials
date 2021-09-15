e23a1c6af5db68f1a3c295379ccf919a14e792fa
Yana <dvoryansakaya.yana@gmail.com>
2021-09-15 13:13:31 +0300

    almost done with scriptableobject

--- New Unity Project/Assets/Scenes/Scripts/Goods.cs
+++ New Unity Project/Assets/Scenes/Scripts/Goods.cs
@@ -1,8 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
-
 
 [CreateAssetMenu(menuName = "Shop/Goods")]
 
@@ -13,4 +13,37 @@ public class Goods : ScriptableObject
 
     public string Name {get => Name; set => _name = value; }
     public int Price { get => _price; set => _price = value; }
+
+    public abstract void Use();
 }
+
+
+//надо вынести этот класс в отдельный файл, я просто забыла это сделать
+[CreateAssetMenu(menuName = "Shop/Eat")]
+public class Eat : Goods
+{
+    public float Mass => _mass;
+    [SerializeField] private float _mass;
+
+    public override void Use()
+    {
+        Debug.Log("game over");
+    }
+}
+
+
+//этот тоже, потому что юнити будет ругаться
+[CreateAssetMenu(menuName = "Shop/Candle")]
+public class Candle : Goods
+{
+    public float BrightRadius => _brightradius;
+    [SerializeField] private float _brightradius;
+    public override void Use()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    
+}
+
+// после того как сделаешь отдельные файлы для них, то в ScriptableObject'ах надо будет указать более кокретные типы (в инспекторе)
